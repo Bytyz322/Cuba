eb4ad49e200b90f656ab96434f094144e8af8a4e
ilyatrofimov <52547175+Bytyz322@users.noreply.github.com>
2020-08-25 15:48:38 +0400

    Add files via upload

---
+++ cuba1/js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/pointer-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/pointer-right.svg" alt=""></button>'
+    });
+});
